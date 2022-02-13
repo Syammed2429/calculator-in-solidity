a82a6533fd90a1de61ae3497410b0999a14b1097
Dada Khalandar <kerry.blig12@gmail.com>
2022-02-13 21:33:24 +0530

    added addition funciton

--- Calculator.sol
+++ Calculator.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.8.11;
 
 contract Calculator {
+    //Declaring Variables
     uint256 a = 29;
     uint256 b = 24;
+
+    //Addition function
+    function Add() external view returns (uint256) {
+        return a + b;
+    }
 }
